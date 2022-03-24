e0fa8419ac41b211aa208b4dfbbce6977244694a
udaykiran176 <86962327+udaykiran176@users.noreply.github.com>
2022-03-24 08:21:23 +0530

    Update bluetooth_arduino.ino

--- bluetooth_arduino.ino
+++ bluetooth_arduino.ino
@@ -11,11 +11,15 @@ void loop()
   if(Serial.available() > 0)  
   {
     Incoming_value = Serial.read();      
-    Serial.print(Incoming_value);        
-    Serial.print("\n");        
-    if(Incoming_value == '1')             
-      digitalWrite(13, HIGH);  
-    else if(Incoming_value == '0')       
-      digitalWrite(13, LOW);   
+    Serial.print("LED stats:");              
+    if(Incoming_value == '1') {            
+      digitalWrite(13, HIGH); 
+      Serial.println("ON");
+    } 
+    else if(Incoming_value == '0')   {    
+      digitalWrite(13, LOW);
+       Serial.println("OFF");
+    }
   }                            
 } 
+
